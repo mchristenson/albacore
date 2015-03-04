14d406799940e32c821f8cff617c8cace4d2ff02
Mark Christenson <markchristenson@livenation.com>
2015-03-04 12:05:38 -0800

    Added ABComponent Model Class

---
+++ albacore/ABModel/ABComponent.h
@@ -0,0 +1,13 @@
+//
+//  ABComponent.h
+//  albacore
+//
+//  Created by Mark Christenson on 3/4/15.
+//  Copyright (c) 2015 Mark Christenson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABComponent : NSObject
+
+@end
---
+++ albacore/ABModel/ABComponent.m
@@ -0,0 +1,13 @@
+//
+//  ABComponent.m
+//  albacore
+//
+//  Created by Mark Christenson on 3/4/15.
+//  Copyright (c) 2015 Mark Christenson. All rights reserved.
+//
+
+#import "ABComponent.h"
+
+@implementation ABComponent
+
+@end
